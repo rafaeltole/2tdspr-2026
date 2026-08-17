d1aa3eb52e476c9a10f49a19673c63e56c038acc
rafael.tolentino <rafael@mini.local>
2026-08-16 23:46:00 -0300

    Material apoio 02-aula

--- 01-semana/02-aula/brasileirao-api/src/main/java/br/com/fiap/brasileirao/PartidaResource.java
+++ 01-semana/02-aula/brasileirao-api/src/main/java/br/com/fiap/brasileirao/PartidaResource.java
@@ -5,39 +5,59 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @RestController
 @RequestMapping("/partidas")
 public class PartidaResource {
 
+    private Random random = new Random();
+    private List<Partida> partidas = new ArrayList<>();
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity cadastrar(@RequestBody Partida novaPartida) {
         System.out.println("Cadastrando Partida ...");
+
+        novaPartida.setCodigo(random.nextLong(1, 100));
+        partidas.add(novaPartida);
+
         return ResponseEntity.status(HttpStatus.CREATED.value()).build();
     }
 
     @PutMapping(path = "/{codigo}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity atualizar(@PathVariable Long codigo, @RequestBody Partida partida) {
+    public ResponseEntity atualizar(@PathVariable Long codigo, @RequestBody Partida partidaAtualizada) {
         System.out.println("Atualizando Partida [" + codigo + "]");
+
+        partidaAtualizada.setCodigo(codigo);
+        int indice = partidas.indexOf(partidaAtualizada);
+
+        Partida partida = partidas.get(indice);
+        partida.setTimeDaCasa(partidaAtualizada.getTimeDaCasa());
+        partida.setGolsTimeDaCasa(partidaAtualizada.getGolsTimeDaCasa());
+
+        partida.setTimeVisitante(partidaAtualizada.getTimeVisitante());
+        partida.setGolsTimeVisitante(partidaAtualizada.getGolsTimeVisitante());
+
         return ResponseEntity.ok().build();
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Partida> consultar() {
+    public ResponseEntity<List<Partida>> consultar() {
         System.out.println("Consultando Partida ...");
-
-        Partida partida = new Partida();
-        partida.setTimeDaCasa("Time da Casa");
-        partida.setGolsTimeDaCasa(2);
-
-        partida.setTimeVisitante("Time Visitante");
-        partida.setGolsTimeVisitante(1);
-
-        return ResponseEntity.ok(partida);
+        return ResponseEntity.ok(partidas);
     }
 
     @DeleteMapping(path = "/{codigo}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity excluir(@PathVariable Long codigo) {
         System.out.println("Excluindo Partida [" + codigo + "]");
+
+        Partida partida = new Partida();
+        partida.setCodigo(codigo);
+
+        partidas.remove(partida);
+
         return ResponseEntity.noContent().build();
     }
 
